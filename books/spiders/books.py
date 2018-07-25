b109a8b75fd0f9d81b1c86bfaa54013089665c6c
Roelandd <roelanddamsma@gmail.com>
2018-07-25 11:56:18 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -2,29 +2,18 @@
 import scrapy
 
 
-class BooksSpider(scrapy.Spider):
-    name = "books"
-    allowed_domains = ["books.toscrape.com"]
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
     start_urls = [
-        'http://books.toscrape.com/',
+        'https://www.belsimpel.nl/samsung-galaxy-s9/zwart/aanbieding?abonnement=t2-100-minuten-2000-mb-4g-2-jaar',
+        'https://www.belsimpel.nl/samsung-galaxy-s9/zwart/aanbieding?abonnement=t2-100-minuten-5000-mb-4g-2-jaar',
+        'https://www.belsimpel.nl/samsung-galaxy-s9/zwart/aanbieding?abonnement=t2-100-minuten-10000-mb-4g-2-jaar',
+        'https://www.belsimpel.nl/samsung-galaxy-s9/zwart/aanbieding?abonnement=t2-onbeperkt-minuten-onbeperkt-mb-4g-2-jaar',
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
-
-    def parse_book_page(self, response):
-        item = {}
-        product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
-        yield item
+        for quote in response.css('div.pd_chosen_offer_cta'):
+            yield {
+                'toestel': quote.css('h1.pd_title::text').extract_first(),
+                'prijs': quote.css('div.pd_offer_picker_row_price::text').extract_first()
+            }
