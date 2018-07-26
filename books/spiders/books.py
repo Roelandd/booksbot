15a73c61dc6b7f0156716dbe09b167db3722af66
Roelandd <roelanddamsma@gmail.com>
2018-07-26 10:05:47 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -10,10 +10,17 @@ class BelsimpelSpider(scrapy.Spider):
         'apple-iphone-8-plus/64gb-zwart/'
     ]
     subscriptions = [
+        't-mobile-normaal-go-next-2-jaar-hsnwa3',
+        'vodafone-red-2-jaar',
+        'vodafone-start-xl-2-jaar',
         't2-100-minuten-2000-mb-4g-2-jaar',
         't2-100-minuten-5000-mb-4g-2-jaar',
         't2-100-minuten-10000-mb-4g-2-jaar',
-        't2-onbeperkt-bellen-onbeperkt-mb-4g-2-jaar'
+        't2-onbeperkt-bellen-onbeperkt-mb-4g-2-jaar',
+        'hollandsnieuwe-6000-mb-min-sms-4g-2-jaar',
+        'telfortnieuw-150-minuten-10000-mb-4g-2-jaar',
+        'kpn-basis-2-jaar',
+        'kpn-zorgeloos-standaard-2-jaar'
     ]
     start_urls = []
 
@@ -25,6 +32,7 @@ class BelsimpelSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css('div.pd_chosen_offer_cta'):
             yield {
+                'url': response.requrest.url
                 'prijs': quote.xpath('/html/body/div[10]/div/div[1]/div[1]/div/div[4]/div[5]/div[1]/text()').extract_first(),
                 'data': quote.xpath('/html/body/div[10]/div/div[1]/div[1]/div/div[4]/div[1]/div[2]/div/div[1]/p/span[1]/text()').extract_first(),
                 'minuten': quote.xpath('/html/body/div[10]/div/div[1]/div[1]/div/div[4]/div[1]/div[2]/div/div[1]/p/span[2]/text()').extract_first(),
