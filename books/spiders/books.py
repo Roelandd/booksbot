23d56cc83d1846da35cf1643d59810f73ca3c0f6
Roelandd <roelanddamsma@gmail.com>
2018-07-27 15:52:25 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -42,8 +42,8 @@ class BelsimpelSpider(scrapy.Spider):
         for quote in response.css('div.pd_chosen_offer_cta'):
             yield {
                 'url': response.request.url,
-                'prijs': quote.xpath('//*[@class="price size_lg"]/text()').extract_first(),
-                'abo': 'FUTURE',
+                'prijs': quote.xpath('//*[@class="pd_offer_picker_row pd_offer_picker_row_hardware_cost"]/*/text()').extract_first(),
+                'abo': 'FUTURE'
                 'provider': quote.xpath('//*[@class="pd_offer_picker_product_images_provider"]/*/@alt').extract_first().lower()[:-11],
                 'toestel': quote.xpath('//*[@id="pd_title"]/text()').extract_first(),
                 'shop': 'belsimpel'
