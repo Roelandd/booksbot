13707298c8a4f720a88036da94c5105468744926
Roelandd <roelanddamsma@gmail.com>
2018-07-27 11:54:44 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -60,12 +60,10 @@ class MobielSpider(scrapy.Spider):
     start_urls = ['https://www.mobiel.nl/abonnement/t-mobile/t-mobile-go-2-jaar?utf8=%E2%9C%93&%5Bmain_bundle%5D=31300&%5Bmax_price_incl_btw%5D=0#']
 
     def parse(self, response):
-        for quote in response.css('body'):
+        for quote in response.css('proposed-phone__row js-phone-proposition'):
             yield {
                 'url': response.request.url,
                 'prijs': quote.xpath('//*[@class="proposed-phone__recurring-price"]/text()').extract_first(),
-#                'data': quote.xpath('//*[@data-scrollpoint_name="inhoud-abonnement"]/ul/li[2]/text()').extract_first(),
-#                'minuten': quote.xpath('//*[@data-scrollpoint_name="inhoud-abonnement"]/ul/li[1]/text()').extract_first(),
                 'provider': 't-mobile',
                 'toestel': quote.xpath('//*[@class="proposed-phone__image-and-name"]/text()').extract_first(),
             }
