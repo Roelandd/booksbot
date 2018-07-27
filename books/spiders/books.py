c478fb7a0634ec51852445dfd7c71e1a48ba6d6d
Roelandd <roelanddamsma@gmail.com>
2018-07-27 11:18:31 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -54,5 +54,4 @@ class GsmwebSpider(scrapy.Spider):
     start_urls = ['https://www.gsmweb.nl/t-mobile&extra_cat=compleet&abo_cat=per%20maand&duration=24%20maanden']
 
     def parse(self, response):
-        rows = response.css('listing')
-        print(rows)
+        print(response)
