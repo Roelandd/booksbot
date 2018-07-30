040bacbb20d981cdaf86746c83e1b5292ea89f22
Roelandd <roelanddamsma@gmail.com>
2018-07-30 13:18:17 +0200

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -6,10 +6,10 @@ class Iphone8_64Spider(scrapy.Spider):
     start_urls = [
         'https://www.belsimpel.nl/apple-iphone-8/64gb-zwart/aanbieding?abonnement=t-mobile-normaal-go-next-2-jaar-hsnwa',
         'https://www.belsimpel.nl/apple-iphone-8/64gb-zwart/aanbieding?abonnement=t2-0-minuten-5000-mb-4g-2-jaar',
-        'https://www.belsimpel.nl/apple-iphone-8/64gb-zwart/aanbieding?abonnement=vodafone-red-iphonex-2-jaar',
+        'https://www.belsimpel.nl/apple-iphone-8/64gb-zwart/aanbieding?abonnement=vodafone-red-2-jaar',
         'https://www.mobiel.nl/abonnement/t-mobile/t-mobile-go-2-jaar?utf8=%E2%9C%93&%5Bmain_bundle%5D=31300&%5Bmax_price_incl_btw%5D=0#',
         'https://www.mobiel.nl/abonnement/tele2/tele2-2-jaar?utf8=%E2%9C%93&%5Bmain_bundle%5D=31234&%5Bvoice_bundle%5D=31237&%5Bmax_price_incl_btw%5D=0#',
-        'https://www.mobiel.nl/abonnement/vodafone/vodafone-2-jaar?utf8=%E2%9C%93&%5Bmain_bundle%5D=31635&%5Bmax_price_incl_btw%5D=0#'
+        'https://www.mobiel.nl/abonnement/vodafone/vodafone-2-jaar?utf8=%E2%9C%93&%5Bmain_bundle%5D=31635&%5Bmax_price_incl_btw%5D=0#',
     ]
     def parse(self, response):
         if 'belsimpel.nl' in response.request.url:
